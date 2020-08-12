9201c32acff69dab1b33ca56fa2bea111f737740
Cronus1007 <lit2019058@iiitl.ac.in>
2020-08-13 05:23:09 +0530

    message

--- Ujja-react-app/src/App.js
+++ Ujja-react-app/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+import "./App.css";
 
 function App() {
+  const { Sider, Header, Content, Footer } = Layout;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout className="height">
+        <Sider className="sider">Sider</Sider>
+        <Layout className="sider">
+          {/* <Header className="sider">Header</Header> */}
+          <div className="div-content">
+            <Content className="content">Content</Content>
+          </div>
+          {/* <Footer className="sider">Footer</Footer> */}
+        </Layout>
+      </Layout>
     </div>
   );
 }
